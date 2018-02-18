4cb4d2a9c5d639a9d3f0f27db03321121db2fc90
Rafa-MKR2 <rafaeldocarmo2010@gmail.com>
2018-02-18 18:29:04 -0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,90 @@
+window.onload = function(){
+	//Constantes que armazenam o código de cada seta do teclado
+	var LEFT = 37, UP = 38, RIGHT = 39, DOWN = 40;
+	
+	var cnv = document.querySelector("canvas");
+	var	ctx = cnv.getContext("2d");
+	var spriteSheet = new Image();
+	spriteSheet.src = "img/sprite.png";
+	var zezim = new Sprite(spriteSheet);
+	var scene = new Image();
+	scene.src = "img/scene.jpg";
+	window.addEventListener("keydown",keydownHandler,false);
+	window.addEventListener("keyup",keyupHandler,false);
+	
+	function keydownHandler(e){
+		switch(e.keyCode){
+			case RIGHT:
+				zezim.mvRight = true;
+				zezim.mvLeft = false;
+				zezim.mvUp = false;
+				zezim.mvDown = false;
+				break;
+			case LEFT:
+				zezim.mvRight = false;
+				zezim.mvLeft = true;
+				zezim.mvUp = false;
+				zezim.mvDown = false;
+				break;
+			case UP:
+				zezim.mvRight = false;
+				zezim.mvLeft = false;
+				zezim.mvUp = true;
+				zezim.mvDown = false;
+				break;
+			case DOWN:
+				zezim.mvRight = false;
+				zezim.mvLeft = false;
+				zezim.mvUp = false;
+				zezim.mvDown = true;
+				break;
+		}
+	}
+	
+	function keyupHandler(e){
+		switch(e.keyCode){
+			case RIGHT:
+				zezim.mvRight = false;
+				break;
+			case LEFT:
+				zezim.mvLeft = false;
+				break;
+			case UP:
+				zezim.mvUp = false;
+				break;
+			case DOWN:
+				zezim.mvDown = false;
+				break;
+		}
+		
+		zezim.posX = Math.max(0, Math.min(cnv.width - zezim.width, zezim.posX));
+		zezim.posY = Math.max(0, Math.min(cnv.height - zezim.height, zezim.posY));
+		
+	}
+	
+	//Quano a imagem é carregada, o programa é iniciado
+	spriteSheet.onload = function(){
+		init();
+		zezim.posX = zezim.posY = 150;
+	}
+
+	function init(){
+		loop();
+	}
+
+	function update(){
+		zezim.move();
+	}
+
+	function draw(){
+		ctx.clearRect(0,0,cnv.width,cnv.height);
+		ctx.drawImage(scene,0,0,scene.width,scene.height,0,0,scene.width,scene.height);
+		zezim.draw(ctx);
+	}
+
+	function loop(){
+		window,requestAnimationFrame(loop,cnv);
+		update();
+		draw();
+	}
+}
